d0f15083d3b08e4e444436017a986e2fb2320202
Paul Solt <PaulSolt@gmail.com>
2020-06-10 14:14:41 -0400

    Parsing LSIQuake assuming all fields ar erequired

--- Quakes-Objc/LSIQuake.m
+++ Quakes-Objc/LSIQuake.m
@@ -37,12 +37,40 @@
     // pull out data from dictionary
     
     // dictionaries/arrays at top as we drill into data
-    
+    NSDictionary *properties = dictionary[@"properties"]; // FIXME: Use constants
+    NSDictionary *geometry = dictionary[@"geometry"];
+    NSArray *coordinates = geometry[@"coordinates"]; // wrong key and it will return nil!
     
     // if valid, create object and return it
     // else (invalid) return nil (failure to parse object)
     
-    return nil;
+    // Create temporary variables
+    NSNumber *magnitudeNumber = properties[@"mag"];
+    NSString *place = properties[@"place"];
+    NSNumber *timeNumber = properties[@"time"];
+    
+    NSNumber *latitude = nil;
+    NSNumber *longitude = nil;
+    
+    if (coordinates.count >= 2) { // prevents an indexof bounds crash! JSON data
+        // longitude is first in array based on API
+        longitude = coordinates[0];
+        latitude = coordinates[1];
+    } else {
+        // failure missing coordinates
+        NSLog(@"Missing coordinates"); // TODO: do something???
+    }
+    
+    // required properties for valid object (not always a safe assumption)
+    if (magnitudeNumber && place && timeNumber && latitude && longitude) {
+        
+        NSDate *time = [[NSDate alloc] initWithTimeIntervalSince1970:timeNumber.longValue / 1000.0];
+        
+        self = [self initWithMagnitude:magnitudeNumber.doubleValue place:place time:time latitude:latitude.doubleValue longitude:longitude.doubleValue];
+        return self;
+    } else { // missing a property that we consider required, so we'll throw out data
+        return nil;
+    }
 }
 
 @end
